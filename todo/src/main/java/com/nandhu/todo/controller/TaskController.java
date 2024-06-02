721e5d284a925660a02f8f932a8fccb8fdba2988
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-02 15:14:56 +0530

    Completed backend and frontend integrated Crud Applictaion

--- todo/src/main/java/com/nandhu/todo/controller/TaskController.java
+++ todo/src/main/java/com/nandhu/todo/controller/TaskController.java
@@ -2,10 +2,7 @@ package com.nandhu.todo.controller;
 
 import com.nandhu.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.nandhu.todo.model.Task;
 
 import java.util.ArrayList;
@@ -13,19 +10,37 @@ import java.util.List;
 import com.nandhu.todo.repository.TaskRepository;
 
 @RestController
+@CrossOrigin // this annotation use for access springboot server in frontend
+@RequestMapping("/api/tasks") //this method use for request mapping to all the post , get don't want to give the path
 public class TaskController {
 
     @Autowired // this annotation use for initialize object
     private TaskRepository taskRepository;
 
-    @GetMapping
+    @GetMapping("/hello-world")
     public String helloWorld(){
         return "hello world";
     }
 
-    @PostMapping("/api/tasks")
+    @PostMapping
     public Task createTask(@RequestBody Task task){
-        taskRepository.save(task);
+        taskRepository.save(task); // this method for save database
         return task;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll(); // this method for get all data for database
+    }
+
+    @PutMapping("/{id}") // this brace value is dynamic so give this path {id}
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task){ // this annotation use for parameter passed id value get this path variable annotation
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
     }
 }
