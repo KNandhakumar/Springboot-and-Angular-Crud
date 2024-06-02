ceb25c8655e6a4f9d3dfb0557deb4975167f5488
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-02 08:58:39 +0530

    create model package then create Task class this is Entity

---
+++ todo/src/main/java/com/nandhu/todo/model/Task.java
@@ -0,0 +1,37 @@
+package com.nandhu.todo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+
+    private Long id;
+    private String description;
+    private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
