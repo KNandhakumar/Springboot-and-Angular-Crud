9b8ede918f386aac37812ded11144ccd92ef9348
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-02 08:42:13 +0530

    Demo Project for Springboot Angular Crud

---
+++ todo/src/test/java/com/nandhu/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandhu.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
